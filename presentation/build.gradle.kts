14ac530540fcf77edd5918997b07e2c9141bed51
arekalov <artyom.rekalov@gmail.com>
2024-03-08 18:12:26 +0300

    edit menu in app module

--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 
 android {
-    namespace = "com.arekalov.presenation"
+    namespace = "com.arekalov.presentation"
     compileSdk = 34
 
     defaultConfig {
@@ -39,6 +39,8 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
+    implementation(libs.androidx.navigation.fragment.ktx)
+    implementation(libs.androidx.navigation.ui.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
