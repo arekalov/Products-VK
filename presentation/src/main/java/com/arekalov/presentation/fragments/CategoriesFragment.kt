760386522a3654098607bf56bdc2b4f7a3606393
arekalov <artyom.rekalov@gmail.com>
2024-03-09 21:43:34 +0300

    add CategoryViewModel.kt

--- presentation/src/main/java/com/arekalov/presentation/fragments/CategoriesFragment.kt
+++ presentation/src/main/java/com/arekalov/presentation/fragments/CategoriesFragment.kt
@@ -6,8 +6,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.arekalov.presentation.R
+import com.arekalov.presentation.adapters.CategoryAdapter
+import com.arekalov.presentation.databinding.FragmentCategoriesBinding
 
 class CategoriesFragment : Fragment() {
+    private lateinit var binding: FragmentCategoriesBinding
+    private lateinit var categoriesAdapter: CategoryAdapter
+    private lateinit var categoryView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
---
+++ presentation/src/main/java/com/arekalov/presentation/viewModels/CategoryViewModel.kt
@@ -0,0 +1,22 @@
+package com.arekalov.presentation.viewModels
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.arekalov.data.network.ProductsRepository
+import kotlinx.coroutines.launch
+
+class CategoryViewModel(private val repository: ProductsRepository): ViewModel() {
+    private val categoriesLiveData = MutableLiveData<List<String>>()
+    suspend fun searchProducts(keyword: String){
+        viewModelScope.launch {
+            val result = repository.getCategories()
+            if (result != null) {
+                categoriesLiveData.value = result!!
+            }
+        }
+    }
+
+    fun observeSearchedViewModel(): LiveData<List<String>> = categoriesLiveData
+}
