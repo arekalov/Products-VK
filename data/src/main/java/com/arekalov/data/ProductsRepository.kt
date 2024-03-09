b5116ac28cbef856113798779a718bcd2f9574c1
arekalov <artyom.rekalov@gmail.com>
2024-03-09 10:40:28 +0300

    edit repository

--- data/src/main/java/com/arekalov/data/ProductsRepository.kt
+++ data/src/main/java/com/arekalov/data/ProductsRepository.kt
@@ -1,15 +1,26 @@
 package com.arekalov.data
 
 import com.arekalov.data.ProductsNetworkService
+import com.arekalov.data.models.Product
+import com.arekalov.data.models.Products
+import retrofit2.Response
 
 class ProductsRepository(
     private val networkService: ProductsNetworkService
 ) {
-    suspend fun getProducts() {
-        return networkService.getProducts()
+    suspend fun getProducts(): List<Product>? {
+        val result = networkService.getProducts()
+        if (result.isSuccessful) {
+            return networkService.getProducts().body()?.products
+        }
+        return null
     }
 
-    suspend fun getProduct(id: String) {
-        return networkService.getProduct(id)
+    suspend fun getProduct(id: String): Product? {
+        val result = networkService.getProduct(id)
+        if (result.isSuccessful) {
+            return networkService.getProduct(id).body()
+        }
+        return null
     }
 }
